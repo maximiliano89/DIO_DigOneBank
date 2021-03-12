2d6a0f3c720d044c6dfc821c25204d626536941a
Maximiliano Arcari <maximiliano.arcari@conduent.com>
2021-03-11 21:32:48 -0300

    Oitavo commit

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/espelho/max/ClienteTipo.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/espelho/max/ClienteTipo.kt
@@ -1,6 +1,6 @@
 package one.digitalinnovation.espelho.max
 
-enum class ClienteTipo(descricao: String) {
+enum class ClienteTipo(val descricao: String) {
     PF( "Pessoa Fisica"),
     PJ( "Pessoa Juridica");
 }
--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/espelho/max/testes/TesteClienteEnun.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/espelho/max/testes/TesteClienteEnun.kt
@@ -3,10 +3,18 @@ package one.digitalinnovation.espelho.max.testes
 import one.digitalinnovation.espelho.max.ClienteTipo
 
 fun main(){
+    //chamando direto o valor no Enum usando o value e o foreach
     ClienteTipo.values().forEach { elemento ->
-        println("${elemento.name} $elemento")
+        println("${elemento.name} = ${elemento.descricao}")
     }
 
     ClienteTipo.values().forEach { it
-    println("${it.name} - $it")}
+    println("${it.name} - ${it.descricao}")}
+
+    //instanciando o Enum em variaveis e acessando seu conteudo pela val declarada
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
 }
