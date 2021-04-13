c79e116e23d478800d0cbafde5103f46d25c6f1b
Maximiliano Arcari <maximiliano.arcari@conduent.com>
2021-04-12 22:36:36 -0300

    Usando mutableSetof e mutableListOf

---
+++ api-collections-max/src/main/kotlin/max/one/digitalinnovation/collections/TesteColecoesMutaveis.kt
@@ -0,0 +1,29 @@
+package max.one.digitalinnovation.collections
+
+fun main(){
+
+    val joao = Funcionario(nome = "Joao", salario = 2000.0, tipoContratacao = "CLT")
+    val pedro = Funcionario(nome = "Pedro", salario = 1500.0, tipoContratacao = "PJ")
+    val maria = Funcionario(nome = "Maria", salario = 4000.0, tipoContratacao = "CLT")
+
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach{ println(it) }
+
+    println("----------------------")
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+
+    println("----------------------")
+    funcionarios.remove(maria)
+    funcionarios.forEach { println(it) }
+
+    println("------------SET----------")
+    val funcionarioSet = mutableSetOf(joao)
+    funcionarioSet.forEach { println(it) }
+
+    println("----------------------")
+
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach { println(it) }
+}
